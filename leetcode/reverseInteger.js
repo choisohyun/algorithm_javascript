de3e222849e32a17611b37de281ba3d43fe6d917
choisohyun <sohyun4283@naver.com>
2020-01-08 15:04:35 +0900

    Update README.md

--- leetcode/reverseInteger.js
+++ leetcode/reverseInteger.js
@@ -6,17 +6,11 @@
 
 const reverse = x => {
     const MAX_NUMBER = Math.pow(2, 31);
-    const absolute = Math.abs(x);
+    const abs_number = Math.abs(x);
 
-    let reverseNumbers = Number(absolute.toString().split('').reverse().join(''));
+    let reverseNumbers = Number(abs_number.toString().split('').reverse().join(''));
     if (reverseNumbers > MAX_NUMBER) return 0;
     let answer = (x < 0) ? -reverseNumbers : reverseNumbers;
 
     return answer;
 }
-
-
-
-console.log(reverse(1534236469));
-console.log(reverse(120));
-console.log(reverse(-123));
